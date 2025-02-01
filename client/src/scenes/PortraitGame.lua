f1fa752fb0839d25c9c6ee8f2d9b7aa71ccaf937
jon12156 <32025765+jon12156@users.noreply.github.com>
2025-02-01 02:20:14 -0700

    Update PortraitGame.lua

--- client/src/scenes/PortraitGame.lua
+++ client/src/scenes/PortraitGame.lua
@@ -202,8 +202,7 @@ function PortraitGame:flipToPortrait()
   end
   -- recreate the global canvas in portrait dimensions
   GAME.globalCanvas = love.graphics.newCanvas(consts.CANVAS_HEIGHT, consts.CANVAS_WIDTH, {dpiscale=GAME:newCanvasSnappedScale()})
-  width, height, _ = love.window.getMode()
-  GAME:handleResize(width, height)
+  GAME:refreshCanvasAndImagesForNewScale()
   
   
 
