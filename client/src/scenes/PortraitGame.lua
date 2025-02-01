e79392f888b72b1285d16bb7de167a193d12ee76
jon12156 <32025765+jon12156@users.noreply.github.com>
2025-02-01 01:15:28 -0700

    window.updateMode before new canvas

--- client/src/scenes/PortraitGame.lua
+++ client/src/scenes/PortraitGame.lua
@@ -193,14 +193,15 @@ function PortraitGame:draw()
 end
 
 function PortraitGame:flipToPortrait()
+  local width, height, _ = love.window.getMode()
+  love.window.updateMode(height, width, {}) --note: love.window.updateMode clears canvases, so let's do this before making a new canvas
+  love.window.setFullscreen(true)
   -- recreate the global canvas in portrait dimensions
   GAME.globalCanvas = love.graphics.newCanvas(consts.CANVAS_HEIGHT, consts.CANVAS_WIDTH, {dpiscale=GAME:newCanvasSnappedScale()})
-
-  local width, height, _ = love.window.getMode()
+  GAME:updateCanvasPositionAndScale(width, height)
+  
   if love.system.getOS() == "Android" or DEBUG_ENABLED then
     -- flip the window dimensions to portrait
-    love.window.updateMode(height, width, {})
-    love.window.setFullscreen(true)
     --GAME:updateCanvasPositionAndScale(width, height)
   end
 
